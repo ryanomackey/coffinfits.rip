77211f8e8b306d6f6ac226728a3bf35d292aa07a
Ryan Mackey <ryan.o.mackey@gmail.com>
2020-03-28 20:35:15 -0500

    fix(Music): add react-helmet to page

--- src/pages/music.js
+++ src/pages/music.js
@@ -3,25 +3,27 @@ import { graphql, Link } from 'gatsby'
 import Img from 'gatsby-image'
 
 import Layout from '../components/Layout'
+import SEO from '../components/SEO'
 
 import './music.css';
 
 const Music = ({ data }) => {
   return (
     <Layout>
+      <SEO title="Music" />
       <div className="music">
-      {data.allAlbum.edges
-        .map(album => {
-          const albumArt = data.allFile.edges.find(file => file.node.parent?.id === album.node.id);
-          
-          return (
-            <Link key={album.node.id} to={`/${album.node.path}`} className="music__link">
-              <Img fluid={albumArt.node.placeholderImage.fluid} />
-              <span className="music__link-name">{album.node.name}</span>
-            </Link>
-          );
-      })}
-    </div>
+        {data.allAlbum.edges
+          .map(album => {
+            const albumArt = data.allFile.edges.find(file => file.node.parent?.id === album.node.id);
+            
+            return (
+              <Link key={album.node.id} to={`/${album.node.path}`} className="music__link">
+                <Img fluid={albumArt.node.placeholderImage.fluid} />
+                <span className="music__link-name">{album.node.name}</span>
+              </Link>
+            );
+        })}
+      </div>
     </Layout>
   )
 }
