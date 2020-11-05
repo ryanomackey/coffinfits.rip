4a88fdc6c506c88c6e7ede3a184481b299cbe564
Ryan O. Mackey <ryan.o.mackey@gmail.com>
2020-11-04 21:34:32 -0600

    feat: add google analytics (#10)

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,15 @@ module.exports = {
         include_favicon: false,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-W86E0Q7MJ3", // Google Analytics / GA
+        ],
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
