08e8c69b21044ea5a3dfc5b28bfe1a690c40c9dc
Ryan O. Mackey <ryan.o.mackey@gmail.com>
2021-11-01 18:46:35 -0500

    chore: update youtube embed link

--- src/components/YouTubeEmbed/YouTubeEmbed.js
+++ src/components/YouTubeEmbed/YouTubeEmbed.js
@@ -5,7 +5,7 @@ import './YouTubeEmbed.css';
 const YouTubeEmbed = () => (
   <iframe
     className="youtube_embed"
-    src="https://www.youtube.com/embed/h4GTLunqCC4"
+    src="https://www.youtube.com/embed/MUbNGtwE8xQ"
     title="YouTube video player"
     frameBorder="0"
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
