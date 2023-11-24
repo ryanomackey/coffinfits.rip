5fa6a227a88368fd9587f0d71a3de4b9301cfa4c
Ryan O. Mackey <ryan.o.mackey@gmail.com>
2023-11-24 16:24:02 -0700

    style: make responsive

--- app/components/Gig/Gig.tsx
+++ app/components/Gig/Gig.tsx
@@ -11,12 +11,12 @@ const Gig = ({ show }: GigProps) => {
   const date = formatInTimeZone(new Date(show.date), 'UTC', `PPPP`);
 
   return (
-    <div className="grid grid-cols-3 py-4">
-      <div className="flex flex-col">
+    <div className="grid grid-cols-1 gap-4 py-4 md:grid-cols-3">
+      <div className="flex flex-col pb-4">
         <span className="text-xl">{date}</span>
         {show.time && <span className="text-sm">{show.time}</span>}
       </div>
-      <div className="flex flex-col">
+      <div className="flex flex-col pb-4">
         <span className="text-xl">{venue.name}</span>
         <div className="text-sm">
           <span className="block">{address.line1}</span>
@@ -26,7 +26,7 @@ const Gig = ({ show }: GigProps) => {
         </div>
       </div>
       {otherBands && (
-        <div className="flex flex-col">
+        <div className="flex flex-col pb-4">
           <span className="text-xl">With our friends</span>
           <span className="text-sm">
             {otherBands.map(({ name }) => name).join(', ')}
